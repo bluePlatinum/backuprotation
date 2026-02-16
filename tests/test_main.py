57f38a8fb28ef8b89edfae423da2d96316c95b6f
bluePlatinum <jukic.rok@gmail.com>
2026-02-16 18:14:23 +0100

    added testcase for file deletion

--- tests/test_main.py
+++ tests/test_main.py
@@ -59,6 +59,27 @@ def create_timed_dir():
     return tmpdir, dirs
 
 
+@pytest.fixture
+def create_timed_files():
+    tmpdir = tempfile.TemporaryDirectory()
+
+    files = list()
+
+    filenum = 30
+
+    for _ in range(filenum):
+        name = "".join(random.choices(string.ascii_lowercase, k=8))
+
+        while os.path.exists(name):
+            name = "".join(random.choices(string.ascii_lowercase, k=8))
+
+        time.sleep(0.1)
+        open(os.path.join(tmpdir.name, name), "w").close()
+        files.append(name)
+
+    return tmpdir, files
+
+
 class TestArgumentParsing:
     def test_required_path(self):
         with pytest.raises(SystemExit):
@@ -127,7 +148,7 @@ class TestRotation:
 
         assert set(dirs) == set(after)
 
-    def test_deletion(self, create_timed_dir):
+    def test_dir_deletion(self, create_timed_dir):
         tmpdir, dirs = create_timed_dir
 
         n = 10
@@ -139,3 +160,16 @@ class TestRotation:
         after = os.listdir(tmpdir.name)
 
         assert set(dirs[-n:]) == set(after)
+
+    def test_file_deletion(self, create_timed_files):
+        tmpdir, files = create_timed_files
+
+        n = 10
+
+        discovered = discover(tmpdir.name, False, True)
+
+        rotate(discovered, n, False)
+
+        after = os.listdir(tmpdir.name)
+
+        assert set(files[-n:]) == set(after)
